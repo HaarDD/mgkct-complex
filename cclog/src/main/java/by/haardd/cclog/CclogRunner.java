5eddcde2fb34f73417b0ba43e5b82e24db1c41d6
HaarDD <maxgalitskiy@gmail.com>
2024-03-08 02:58:14 +0300

    init commit

---
+++ cclog/src/main/java/by/haardd/cclog/CclogRunner.java
@@ -0,0 +1,13 @@
+package by.haardd.cclog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CclogRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CclogRunner.class, args);
+    }
+
+}
