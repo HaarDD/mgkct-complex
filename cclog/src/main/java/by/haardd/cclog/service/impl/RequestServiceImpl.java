925139b2e834e3dc26525c41d14a95982ba213cd
HaarDD <maxgalitskiy@gmail.com>
2024-03-10 20:12:29 +0300

    feat: basic services (only status & request, few methods)

---
+++ cclog/src/main/java/by/haardd/cclog/service/impl/RequestServiceImpl.java
@@ -0,0 +1,53 @@
+package by.haardd.cclog.service.impl;
+
+import by.haardd.cclog.dto.RequestDto;
+import by.haardd.cclog.exception.types.ResourceNotFoundException;
+import by.haardd.cclog.mapper.RequestMapper;
+import by.haardd.cclog.repository.RequestRepository;
+import by.haardd.cclog.service.RequestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class RequestServiceImpl implements RequestService {
+
+    private final RequestRepository requestRepository;
+    private final RequestMapper requestMapper;
+
+    @Override
+    public List<RequestDto> getAllByPageable(Pageable pageable) {
+        return requestRepository.findAll(pageable).stream().map(requestMapper::toDto).toList();
+    }
+
+    @Override
+    public RequestDto getById(Long id) {
+        return requestMapper.toDto(requestRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Request was not found", id)));
+    }
+
+    @Override
+    @Transactional
+    public RequestDto save(RequestDto requestDto) {
+
+        requestRepository.save(requestMapper.toEntity(requestDto));
+
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public RequestDto update(RequestDto requestDto, Long id) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+
+    }
+}
---
+++ cclog/src/main/java/by/haardd/cclog/service/impl/StatusServiceImpl.java
@@ -0,0 +1,50 @@
+package by.haardd.cclog.service.impl;
+
+import by.haardd.cclog.dto.StatusDto;
+import by.haardd.cclog.exception.types.ResourceNotFoundException;
+import by.haardd.cclog.mapper.StatusMapper;
+import by.haardd.cclog.repository.StatusRepository;
+import by.haardd.cclog.service.StatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StatusServiceImpl implements StatusService {
+
+    private final StatusRepository statusRepository;
+    private final StatusMapper statusMapper;
+
+    @Override
+    public List<StatusDto> getAllByPageable(Pageable pageable) {
+        return statusRepository.findAll(pageable).stream().map(statusMapper::toDto).toList();
+    }
+
+    @Override
+    public StatusDto getById(Long id) {
+        return statusMapper.toDto(statusRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Status was not found", id)));
+    }
+
+    @Override
+    @Transactional
+    public StatusDto save(StatusDto statusDto) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public StatusDto update(StatusDto statusDto, Long id) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+
+    }
+}
