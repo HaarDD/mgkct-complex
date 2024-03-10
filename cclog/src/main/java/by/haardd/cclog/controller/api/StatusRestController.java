66ea2e0bd211916053a52c851afab5735922b036
HaarDD <maxgalitskiy@gmail.com>
2024-03-10 20:11:15 +0300

    feat: add test controllers

---
+++ cclog/src/main/java/by/haardd/cclog/controller/api/StatusRestController.java
@@ -0,0 +1,35 @@
+package by.haardd.cclog.controller.api;
+
+import by.haardd.cclog.config.pageable.OffsetLimitPageable;
+import by.haardd.cclog.dto.StatusDto;
+import by.haardd.cclog.service.StatusService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/statuses")
+@RequiredArgsConstructor
+@Slf4j
+@Tag(name = "Statuses", description = "Status control")
+public class StatusRestController {
+
+    private final StatusService statusService;
+
+    @GetMapping
+    private List<StatusDto> getAll(@RequestParam(defaultValue = "10") Integer limit, @RequestParam(defaultValue = "0") Long offset){
+        return statusService.getAllByPageable(OffsetLimitPageable.of(offset < 0 ? 0L : offset, limit > 10 ? 10 : limit, Sort.by(Sort.Direction.ASC, "id")));
+    }
+
+
+
+
+
+}
