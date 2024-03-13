490762f0aeab2302cba70184b6ef5bd636deacd9
HaarDD <maxgalitskiy@gmail.com>
2024-03-13 04:50:02 +0300

    feat: role, user & user statuses repositories

--- cclog/src/main/java/by/haardd/cclog/repository/RoleRepository.java
+++ cclog/src/main/java/by/haardd/cclog/repository/RoleRepository.java
@@ -1,7 +1,13 @@
 package by.haardd.cclog.repository;
 
 import by.haardd.cclog.entity.Role;
+import by.haardd.cclog.entity.enums.RoleNameEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(RoleNameEnum roleNameEnum);
+
 }
