010b6d2479856bac7cce9fa2d3a7adf7b2929ed4
HaarDD <maxgalitskiy@gmail.com>
2024-03-14 07:37:34 +0300

    feat: find by name method

--- cclog/src/main/java/by/haardd/cclog/repository/StatusRepository.java
+++ cclog/src/main/java/by/haardd/cclog/repository/StatusRepository.java
@@ -1,7 +1,13 @@
 package by.haardd.cclog.repository;
 
 import by.haardd.cclog.entity.Status;
+import by.haardd.cclog.entity.enums.StatusEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StatusRepository extends JpaRepository<Status, Long> {
+
+    Optional<Status> findByName(StatusEnum roleNameEnum);
+
 }
