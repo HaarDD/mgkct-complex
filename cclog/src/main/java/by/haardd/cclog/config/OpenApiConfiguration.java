d6bca885f186447efab3d22afd52524e8d4320aa
HaarDD <maxgalitskiy@gmail.com>
2024-03-21 23:15:43 +0300

    feat: openAPI with security config

--- cclog/src/main/java/by/haardd/cclog/config/OpenApiConfiguration.java
+++ cclog/src/main/java/by/haardd/cclog/config/OpenApiConfiguration.java
@@ -1,24 +1,25 @@
 package by.haardd.cclog.config;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(
+        title = "CClog",
+        version = "1.0",
+        description = "Computer center logging system",
+        contact = @Contact(name = "Maxim Galitskiy")),
+        security = {@SecurityRequirement(name = "bearerToken"), @SecurityRequirement(name = "cookie")})
+@SecuritySchemes({
+        @SecurityScheme(name = "cookie", type = SecuritySchemeType.APIKEY, in = SecuritySchemeIn.HEADER, paramName = "cookie")
+})
 public class OpenApiConfiguration {
 
-    @Bean
-    OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(new Info().title("CClog")
-                        .description("Сервис журналирования заявок")
-                        .version("v0.0.1")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("github")
-                        .url("https://github.com/"));
-    }
 }
